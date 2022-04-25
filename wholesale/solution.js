efb080d7daae1066ecf0774c21fc60d1c09ab229
Apoorv <apoorv11@live.com>
2022-04-25 22:02:50 +0530

    Solution to problem 3(wholesale)

---
+++ wholesale/solution.js
@@ -0,0 +1,59 @@
+var UnitPrices = {
+    Carrot: 10,
+    Apple: 200,
+  };
+  
+  var Discounts = {
+    // values are percentages
+    Apple: 10,
+  };
+  
+  var Taxes = {
+    // values are percentages
+    Carrot: 5,
+    Guava: 10,
+  };
+  
+  var Bills = [
+    [
+      {
+        item: 'Carrot',
+        units: 200,
+      },
+      {
+        item: 'Apple',
+        units: 150,
+      },
+      {
+        item: 'Guava',
+        units: 40,
+      },
+    ],
+    [
+      {
+        item: 'Carrot',
+        units: 20,
+      },
+    ],
+    [
+      {
+        item: 'Apple',
+        units: 30,
+      },
+      {
+        item: 'Guava',
+        units: 80,
+      },
+    ]
+  ];
+  
+  var PaymentsMade = [
+    10000,
+    3000,
+    7500,
+  ]
+  
+  /* Programme */
+  var PendingPayments = [
+    // Compute the payments for every bill and add it here.
+  ];
